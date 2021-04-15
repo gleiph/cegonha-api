8d41ec04db7bd118b236da04976a524175cedd42
walkiria <walkiria.garcia@engenharia.ufjf.br>
2021-04-15 17:07:45 -0300

    initial database

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const carsRoute = require('./routes/Cars')
 
 // ==> Rotas da API:
-const index = require('./routers/index');
+const index = require('./routes/index');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.json({type: 'application/vnd.api+json'}));
 app.use(cors({origin: true, credentials: true}));
 
 app.use(index);
+app.use('/', carsRoute);
 
 module.exports = app;
