29d0081ecb546153bc99eea0bf223860e68c72aa
walkiria <walkiria.garcia@engenharia.ufjf.br>
2021-04-18 20:42:07 -0300

    retirada de autenticação para listar centro médico, centro médico por ID e centro médico por nome

--- src/routes/MedicalCenter.js
+++ src/routes/MedicalCenter.js
@@ -5,9 +5,9 @@ const AuthController = require("../controllers/AuthController");
 const MedicalCenterController = require("../controllers/MedicalCenter")
 
 router.post("/medical-center", AuthController.admin, MedicalCenterController.create);
-router.get("/medical-center", AuthController.auth, MedicalCenterController.all);
-router.get("/medical-center/:id", AuthController.auth, MedicalCenterController.findById);
-router.get("/medical-center-name/:name", AuthController.auth, MedicalCenterController.findByName);
+router.get("/medical-center", MedicalCenterController.all);
+router.get("/medical-center/:id", MedicalCenterController.findById);
+router.get("/medical-center-name/:name", MedicalCenterController.findByName);
 router.put("/medical-center/:id",  AuthController.admin, MedicalCenterController.updateById);
 router.delete("/medical-center/:id",  AuthController.admin, MedicalCenterController.deleteById);
 
