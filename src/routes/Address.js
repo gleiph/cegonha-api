e83b3f2b48199cf5369941eaf4501b6d0f785c48
walkiria <walkiria.garcia@engenharia.ufjf.br>
2021-04-18 20:40:47 -0300

    retirada de autenticação para busca de listar endereços e endereço por id

--- src/routes/Address.js
+++ src/routes/Address.js
@@ -5,8 +5,8 @@ const AuthController = require("../controllers/AuthController");
 const AddressController = require("../controllers/AdressController")
 
 router.post("/address",AuthController.auth, AddressController.create);
-router.get("/address", AuthController.auth, AddressController.all);
-router.get("/address/:id", AuthController.auth, AddressController.findById);
+router.get("/address", AddressController.all);
+router.get("/address/:id", AddressController.findById);
 router.put("/address/:id", AuthController.auth, AddressController.updateById);
 router.delete("/address/:id", AuthController.admin, AddressController.deleteById);
 router.post("/find-center-medical", AddressController.findCenterMedical);
