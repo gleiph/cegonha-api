37a3ae6a9931740ed9a0e170c2bc739083757a53
walkiria <walkiria.garcia@engenharia.ufjf.br>
2021-05-05 00:10:49 -0300

    Recuperação de senha e redefinição de senha

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -54,14 +54,6 @@ const User = sequelize.define("user", {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  passwordResetToken: {
-    type: DataTypes.STRING,
-    select: false,
-  },
-  passwordResetExpires: {
-    type: DataTypes.STRING,
-    select: false,
-  },
 
 });
 
